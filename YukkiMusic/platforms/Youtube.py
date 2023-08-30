595b5f3000daa3a360ba3dc68784ee600b0e6701
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-30 11:26:46 +0530

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -12,8 +12,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from InsaneMusic.utils.database import is_on_off
-from InsaneMusic.utils.formatters import time_to_seconds
+from YukkiMusic.utils.database import is_on_off
+from YukkiMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
